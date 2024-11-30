a16d7fc9e31254ba1b16864b13eefc5dd99175c2
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2024-11-30 12:10:46 +0900

    초기 커밋.

---
+++ src/Client/DMSample.Client.ServerMain/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DMSample.Client.ServerMain
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+        }
+
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            Current.DispatcherUnhandledException += AppDispatcherUnhandledException;
+        }
+
+        private void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+        }
+
+    }
+}
---
+++ src/Server/DMSample.Server/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMSample.Server
+{
+    public class Class1
+    {
+    }
+}
